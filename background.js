429238073217f847d55fc933ac56070acbb038ab
Mixs279 <mychofamadico16@gmail.com>
2025-04-12 13:35:36 +0800

    background.js

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	if (message.action === 'openInIncognito' && message.url) {
+    if (message.action === 'openInIncognito' && message.url) {
 
-		chrome.windows.create({
-			url: message.url,
-			incognito: true
-		}, (window) => {
-			if (chrome.runtime.lastError) {
-				console.error("Error opening incognito window: ", chrome.runtime.lastError);
-			}
-		});
-	}
+        chrome.windows.create({
+            url: message.url,
+            incognito: true
+        }, (window) => {
+            if (chrome.runtime.lastError) {
+                console.error("Error opening incognito window: ", chrome.runtime.lastError);
+            }
+        });
+    }
 });
